2bec203c49709b1bc7297de772179cfeb08dd174
djfried <dougfrieders@gmail.com>
2015-02-01 11:35:43 -0500

    Initial Project setup

---
+++ Capstone/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Capstone.Startup))]
+namespace Capstone
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
