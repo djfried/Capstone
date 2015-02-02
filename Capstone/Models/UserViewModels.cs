c8875d8acda2b3ac9ef1b143bb505b5005cca5f5
Daniel Siebert <dsiebert@knights.ucf.edu>
2015-02-02 16:23:23 -0500

    Added User and Event Classes

---
+++ Capstone/Models/UserViewModels.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Capstone.Models
+{
+
+    public class User
+    {
+        string Username;
+        string Password;
+        string FirstName;
+        string LastName;
+        string Address1;
+        string Address2;
+        string City;
+        string State;
+        int Zip;
+        string PhoneHome;
+        string PhoneCell;
+        string CompanyName;
+        string BranchLocation;
+        int Food_ID;
+        string AdditionalInfo;
+    }
+
+    public class UserViewModels
+    {
+        public User GetMyProfile { get; set; }
+
+        public User GetUserByID { get; set; }
+    }
+}
