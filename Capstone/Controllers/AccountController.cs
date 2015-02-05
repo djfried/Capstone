110e4e34dfa1f7cf17dc11277c1ca5782e5ba4db
Daniel Siebert <dsiebert@knights.ucf.edu>
2015-02-04 20:57:39 -0500

    push pull idk what I did

--- Capstone/Controllers/AccountController.cs
+++ Capstone/Controllers/AccountController.cs
@@ -75,6 +75,8 @@ namespace Capstone.Controllers
                 return View(model);
             }
 
+            //DANN
+
             // This doesn't count login failures towards account lockout
             // To enable password failures to trigger account lockout, change to shouldLockout: true
             var result = await SignInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, shouldLockout: false);
@@ -186,6 +188,7 @@ namespace Capstone.Controllers
                 containerUser.Zip = model.Zip;
 
                 Capstone.Models.UserViewModels userModel = userManager.CreateUser(containerUser);
+                SessionManager.SessionID = userModel.User.ID;
                 return RedirectToAction("Index", "Home");
 
                 // END OF DAN'S CODE
---
+++ Capstone/Managers/SessionManager.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Capstone.Managers
+{
+    public static class SessionManager
+    {
+        public static int SessionID { get; set; }
+    }
+}
